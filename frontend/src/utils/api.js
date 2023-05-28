45338e32bb86722802036af466df3bb501a4b65b
Aleksey Abamzarov <alekseydevweb@gmail.com>
2023-05-28 14:31:35 +0300

    fix: server url in api

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -28,7 +28,7 @@ class Api {
 
   async editUser(data) {
     const res = await fetch(`${this._baseUrl}users/me`, {
-      method: 'PATCH',
+      method: "PATCH",
       headers: this._headers,
       body: JSON.stringify({
         name: data.name,
@@ -40,7 +40,7 @@ class Api {
 
   async editUserAvatar(data) {
     const res = await fetch(`${this._baseUrl}users/me/avatar`, {
-      method: 'PATCH',
+      method: "PATCH",
       headers: this._headers,
       body: JSON.stringify({
         avatar: data.avatar,
@@ -51,7 +51,7 @@ class Api {
 
   async postCard(data) {
     const res = await fetch(`${this._baseUrl}cards`, {
-      method: 'POST',
+      method: "POST",
       headers: this._headers,
       body: JSON.stringify({
         name: data.name,
@@ -63,7 +63,7 @@ class Api {
 
   async deleteCard(cardId) {
     const res = await fetch(`${this._baseUrl}cards/${cardId}`, {
-      method: 'DELETE',
+      method: "DELETE",
       headers: this._headers,
     });
     return this._requestResult(res);
@@ -72,13 +72,13 @@ class Api {
   async changeLikeCardStatus(cardId, isLiked) {
     if (isLiked) {
       const res = await fetch(`${this._baseUrl}cards/${cardId}/likes`, {
-        method: 'PUT',
+        method: "PUT",
         headers: this._headers,
       });
       return this._requestResult(res);
     } else {
       const res = await fetch(`${this._baseUrl}cards/${cardId}/likes`, {
-        method: 'DELETE',
+        method: "DELETE",
         headers: this._headers,
       });
       return this._requestResult(res);
@@ -87,9 +87,9 @@ class Api {
 }
 
 export const api = new Api({
-  serverUrl: 'https://mesto.nomoreparties.co/v1/cohort-60/',
+  serverUrl: "https://api.endjoys.project.nomoredomains.rocks",
   headers: {
-    authorization: '6a189007-b75f-4743-b5a0-635bc162974c',
-    'Content-Type': 'application/json',
+    authorization: "6a189007-b75f-4743-b5a0-635bc162974c",
+    "Content-Type": "application/json",
   },
 });
